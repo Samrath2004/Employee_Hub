3c1361f53fdc266a34aba885737d56dab2951b39
Samrath2004 <samrathagarwal2004@gmail.com>
2025-05-24 23:00:02 +0530

    updated ep

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,15 +1,51 @@
+// import { User } from "../models/userSchema.js";
+// import { catchAsyncErrors } from "./catchAsyncError.js";
+// import ErrorHandler from "./error.js";
+// import jwt from "jsonwebtoken";
+
+// export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+//   const { token } = req.cookies;
+//   if (!token) {
+//     return next(new ErrorHandler("User Not Authorized", 401));
+//   }
+//   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+//   req.user = await User.findById(decoded.id);
+
+//   next();
+// });
+
+
+
+
+
 import { User } from "../models/userSchema.js";
 import { catchAsyncErrors } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token;
+
+  // Check token in cookies
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+  // Or check token in Authorization header (Bearer token)
+  else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  // If no token found, return unauthorized
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
+  // Verify token and attach user to request object
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
 
   next();
